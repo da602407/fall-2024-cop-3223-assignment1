1fd85b426b158338adf357d17ddba57299e09dba
da602407 <da602407@ucf.edu>
2024-09-09 01:08:54 +0000

    testing something

---
+++ Calculate_Distance.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+# include <math.h>
+
+
+int main(){
+
+bussy = pow(3,2);
+printf("%f", &bussy);
+
+
+}
--- chatGPT.c
+++ chatGPT.c
@@ -1,39 +1,83 @@
 #include <stdio.h>
 #include <math.h>
+
 #define PI 3.14159
 
-double calculate_Distance();
-// double calculate_Perimeter();
-// double calculate_Area();
-// double calculate_Width();
-// double calculate_Height();
+// Function Prototypes
+double calculateDistance();
+double calculatePerimeter();
+double calculateArea();
+double calculateWidth();
+double calculateHeight();
 
-int main(int argc, char** argv) {
-    calculate_Distance();
-    // calculate_Perimeter();
-    // calculate_Area();
-    // calculate_Width();
-    // calculate_Height();
-
+// Main function
+int main(int argc, char **argv) {
+    calculateDistance();
+    calculatePerimeter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
     return 0;
 }
 
-double calculate_Distance() {
-    double x_1, y_1, x_2, y_2;
+// Function to calculate distance between two points
+double calculateDistance() {
+    double x1, y1, x2, y2;
+    printf("Enter x1: ");
+    scanf("%lf", &x1);
+    printf("Enter y1: ");
+    scanf("%lf", &y1);
+    printf("Enter x2: ");
+    scanf("%lf", &x2);
+    printf("Enter y2: ");
+    scanf("%lf", &y2);
 
-    // User input of which points will be used
-    printf("Enter x_1: ");
-    scanf("%lf", &x_1);
-    printf("Enter y_1: ");
-    scanf("%lf", &y_1);
-    printf("Enter x_2: ");
-    scanf("%lf", &x_2);
-    printf("Enter y_2: ");
-    scanf("%lf", &y_2);
-
-    double distance_without_sqrt = pow(x_2 - x_1, 2) + pow(y_2 - y_1, 2);
-    double distance = sqrt(distance_without_sqrt);
-    printf("The distance between the two points is equal to %.2lf\n", distance);
+    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    printf("Point #1 entered: x1 = %.2f; y1 = %.2f\n", x1, y1);
+    printf("Point #2 entered: x2 = %.2f; y2 = %.2f\n", x2, y2);
+    printf("The distance between the two points is %.2f\n", distance);
 
     return distance;
+}
+
+// Function to calculate the perimeter of the circle
+double calculatePerimeter() {
+    double radius = calculateDistance() / 2.0;
+    double perimeter = 2 * PI * radius;
+    printf("Point #1 entered: x1 = -; y1 = -\n");
+    printf("Point #2 entered: x2 = -; y2 = -\n");
+    printf("The perimeter of the city encompassed by your request is %.2f\n", perimeter);
+    
+    return 3.0; // Sample difficulty rating; replace with actual difficulty
+}
+
+// Function to calculate the area of the circle
+double calculateArea() {
+    double radius = calculateDistance() / 2.0;
+    double area = PI * pow(radius, 2);
+    printf("Point #1 entered: x1 = -; y1 = -\n");
+    printf("Point #2 entered: x2 = -; y2 = -\n");
+    printf("The area of the city encompassed by your request is %.2f\n", area);
+    
+    return 3.0; // Sample difficulty rating; replace with actual difficulty
+}
+
+// Function to calculate the width (diameter) of the circle
+double calculateWidth() {
+    double diameter = calculateDistance();
+    printf("Point #1 entered: x1 = -; y1 = -\n");
+    printf("Point #2 entered: x2 = -; y2 = -\n");
+    printf("The width of the city encompassed by your request is %.2f\n", diameter);
+    
+    return 2.0; // Sample difficulty rating; replace with actual difficulty
+}
+
+// Function to calculate the height (diameter) of the circle
+double calculateHeight() {
+    double diameter = calculateDistance();
+    printf("Point #1 entered: x1 = -; y1 = -\n");
+    printf("Point #2 entered: x2 = -; y2 = -\n");
+    printf("The height of the city encompassed by your request is %.2f\n", diameter);
+    
+    return 2.0; // Sample difficulty rating; replace with actual difficulty
 }

d3709c5bb810ebed6a3c6279cd65a98241b99db8
da602407 <da602407@ucf.edu>
2024-09-09 21:45:50 +0000

    I believe this is the end

--- fracturing.c
+++ fracturing.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <math.h>
+#include <math.h> // The math library thing gave me such a stresfful time
 #define PI 3.14159
 
 
@@ -51,13 +51,55 @@ double calculate_Perimeter(){
 
 // Area of a circle is = π * r^2
 // Thats all good in theory but now that means I have to find the radius. Which is just diamter/2 but this has to be included in my function
-// 
 double calculate_Area(){
     double x_1, y_1, x_2, y_2;
 
      askForUserInput(&x_1, &y_1, &x_2, &y_2);
      
-     double area = PI * pow((sqrt(pow(x_2 - x_1, 2) + pow(y_2 - y_1, 2)))/2, 2)
+     // I couldnt load the whole eqation into double area so I sperated it into two
+     double radius = sqrt(pow(x_2 - x_1, 2) + pow(y_2 - y_1, 2)) / 2;
+     double area = PI * pow(radius, 2);
+     printf("Point #1 entered: x_1 = %.1f; y_1 = %.1f\n", x_1, y_1);
+     printf("Point #2 entered: x_2 = %.1f; y_2 = %.1f\n", x_2, y_2);
+     printf("The area of the city encompassed by your request is %.2f\n", area);
+
+     return 4.0; // looked intimadting at first but once i seperated it into two variables one for finding the radius and the other for the area, it worked. SO HAPPY!
+
+}
+// Kind of confused on this one because the width of a circle is just the diameter, right?
+// Width of a circle is = 2 * r.  AND   Diamter= 2 * r
+// So I don't know if you are trying to trick me but i feel like I can just use the distance formula again
+double calculate_Width(){
+    double x_1, y_1, x_2, y_2;
+     
+     askForUserInput(&x_1, &y_1, &x_2, &y_2);
+     
+     double width = sqrt(pow(x_2 - x_1, 2) + pow(y_2 - y_1, 2));
+     printf("Point #1 entered: x_1 = %.1f; y_1 = %.1f\n", x_1, y_1);
+     printf("Point #2 entered: x_2 = %.1f; y_2 = %.1f\n", x_2, y_2);
+     printf("The width of the city encompassed by your request is %.2f\n", width);
+
+     return 1.0; // if this is a trick then its a 5.0
+
+}
+// I hope this is correct because I thought the height of a circle would just be the diameter.
+// But when I consulted with my good buddy ChatGPT it said that Height = y_2 - y_1
+// So I hope that works and is the correct height formula you are looking for in this assignment
+
+
+double calculate_Height(){
+    double x_1, y_1, x_2, y_2;
+    
+     askForUserInput(&x_1, &y_1, &x_2, &y_2);
+
+     double height = y_2 - y_1;
+     printf("Point #1 entered: x_1 = %.1f; y_1 = %.1f\n", x_1, y_1);
+     printf("Point #2 entered: x_2 = %.1f; y_2 = %.1f\n", x_2, y_2);
+     printf("The height of the city encompassed by your request is %.2f\n", height);
+
+     return 5.0;
+
+
 
 }
 
@@ -65,8 +107,8 @@ int main (int argc, char **argv){
     calculate_Distance();
     calculate_Perimeter();
     calculate_Area();
-    //calculate_Width();
-    //calculate_Height();
+    calculate_Width();
+    calculate_Height();
 
 
     return 0;

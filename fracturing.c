b10974b9e61da3f3e08403d903bd47adb8a9c25e
da602407 <da602407@ucf.edu>
2024-09-09 16:10:21 +0000

    i am finally cooking

--- fracturing.c
+++ fracturing.c
@@ -1,55 +1,73 @@
-#include <math.h>
 #include <stdio.h>
+#include <math.h>
 #define PI 3.14159
 
-
-double calculate_Distance();
-//double calculate_Perimeter();
-//double calculate_Area();
-//double calculate_Width();
-//double calculate_Height();
-
-
+// Need to write a function that uses user input to generate 2 points and from there find the distance between the two points.
+// To find the distance between two points we use the distance formula = √(x_2-x_1)^2 + (y_2-y_1)^2
 double calculate_Distance(){
-   double x_1, y_1, x_2, y_2;
+    double x_1, y_1, x_2, y_2;
+    
+     printf("Enter x_1:");
+     scanf("%lf", &x_1);
+     printf("Enter y_1:");
+     scanf("%lf", &y_1);
+     printf("Enter x_2:");
+     scanf("%lf", &x_2);
+     printf("Enter y_2:");
+     scanf("%lf", &y_2);
 
-   // User input of which points will be used
-   printf("Enter x_1:");
-   scanf("%lf", &x_1);
-   printf("Enter y_1:");
-   scanf("%lf", &y_1);
-   printf("Enter x_2:");
-   scanf("%lf", &x_2);
-   printf("Enter y_2:");
-   scanf("%lf", &y_2);
+    double distance = sqrt(pow(x_2 - x_1, 2) + pow(y_2 - y_1, 2));
+    printf("Point #1 entered: x_1 = %.1f; y_1 = %.1f\n", x_1, y_1);
+    printf("Point #2 entered: x_2 = %.1f; y_2 = %.1f\n", x_2, y_2);
+    printf("The distance between the two points is %.2f\n", distance); 
+}
 
-   int main(){
-   double result = sqrt(25);
-   printf("The square root of 25 is equal to %.2f\n", result);
-   return 0;
+// Perimeter of a circle is the same thing as the circumference of a circle
+// So the formula for the circumference of a circle = π * diameter
+// We have the diamter already from calcualting the distance so all that is left to do is multiply it by π
+double calculate_Perimeter(){
+    double x_1, y_1, x_2, y_2;
+     printf("Enter x_1:");
+     scanf("%lf", &x_1);
+     printf("Enter y_1:");
+     scanf("%lf", &y_1);
+     printf("Enter x_2:");
+     scanf("%lf", &x_2);
+     printf("Enter y_2:");
+     scanf("%lf", &y_2);
 
-   }
+     double perimeter = PI * sqrt(pow(x_2 - x_1, 2) + pow(y_2 - y_1, 2));
+     printf("Point #1 entered: x_1 = %.1f; y_1 = %.1f\n", x_1, y_1);
+     printf("Point #2 entered: x_2 = %.1f; y_2 = %.1f\n", x_2, y_2);
+     printf("The perimeter of the city encompassed by your request is %.2f\n", perimeter);
 
-
-
-   //double distance = sqrt(pow(x_2 - x_1, 2) + pow(y_2 - y_1, 2));
-
-   //printf("Point #1 entered: x_1 = %.2f; y_1 = %.2f\n", x_1, y_1);
-   //printf("Point #2 entered: x_2 = %.2f; y_2 = %.2f\n", x_2, y_2);
-   //printf("The distance between the two points is %.2f\n", distance);
-
-   //return distance;
-
+     return 4.0;
 
 }
 
-//int main (int argc, char** argv){
+
+//double calculate_Area(){
+    //double x_1, y_1, x_2, y_2;
+     //printf("Enter x_1:");
+     //scanf("%lf", &x_1);
+     //printf("Enter y_1:");
+     //scanf("%lf", &y_1);
+     //printf("Enter x_2:");
+     //scanf("%lf", &x_2);
+     //printf("Enter y_2:");
+    // scanf("%lf", &y_2);
+
+     //double area = 
+
+
+
+int main (int argc, char **argv){
     calculate_Distance();
-    //calculate_Perimeter();
+    calculate_Perimeter();
     //calculate_Area();
     //calculate_Width();
-   //calculate_Height();
+    //calculate_Height();
 
-    //return 0;
+
+    return 0;
 }
-
